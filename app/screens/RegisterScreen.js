9eafac82d45e89e8c0e857f9fc8a5c94bfc2779f
fabriccio27 <fabricio_inc27@hotmail.com>
2021-06-19 20:32:51 -0300

    added password input and events, is working

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import  {View, Text} from 'react-native';
 import Input from "../components/Input";
 
 
 function RegisterScreen() {
+    const [changedText, setChangedText] = useState("");
+    const [submittedText, setSubmittedText] = useState("");
+
     return (
         <View>
             <Text>Register Screen babyyy</Text>
             <Input label="Basic Text Input:" />
-            {/* <Input label="Password Input:" secureTextEntry />
-            <Input label="Return Key:" returnKeyType="search" />
-            <Input label="Placeholder Text:" placeholder="Search" />
+            <Input label="Password Input:" secureTextEntry />
+            
+            
             <Input
-            label="Input Events:"
-            onChangeText={e => {
-            setChangedText(e);
-            }}
-            onSubmitEditing={e => {
-            setSubmittedText(e.nativeEvent.text);
-            }}
-            onFocus={() => {
-            setChangedText("");
-            setSubmittedText("");
-            }}
+                label="Input Events:"
+                onChangeText={e => {
+                    setChangedText(e);
+                }}
+                onSubmitEditing={e => {
+                    setSubmittedText(e.nativeEvent.text);
+                }}
+                onFocus={() => {
+                    setChangedText("");
+                    setSubmittedText("");
+                }}
+                
             />
             <Text>Changed: {changedText}</Text>
-            <Text>Submitted: {submittedText}</Text> */}
+            <Text>Submitted: {submittedText}</Text>
         </View>
     );
 }
+/* este onFocus me limpia los Text de abajo */
 export default RegisterScreen;
