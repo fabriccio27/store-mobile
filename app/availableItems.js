0b0fc14e650721185f74209c6b19b76dde7218dd
Fabricio Saavedra <fabricio_inc27@hotmail.com>
2021-06-20 12:44:32 -0300

    added loading.js and container style in appStyles.js

---
+++ app/availableItems.js
@@ -0,0 +1,8 @@
+const items = [
+    { id: 1, value: 4, description:"Tomates", price:120 },
+    { id: 2, value: 0, description:"Galletitas", price:250 },
+    { id: 3, value: 0, description:"Lata de atun", price:380 },
+    { id: 4, value: 0, description:"Aquarius", price:170 },
+]
+
+export default items;
---
+++ app/utils/loading.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { View, ActivityIndicator } from "react-native";
+import styles from "../appStyles";
+
+export default function loading(Wrapped) {
+    return function LoadingWrapper(props) {
+
+        const [loading, setLoading] = useState(true);
+
+        useEffect(() => {
+            props.promise.then(() => setLoading(false), () => setLoading(false));
+        },[]);
+
+        if (loading) {
+            return (
+                <View style={styles.container}>
+                    <ActivityIndicator size="large" />
+                </View>
+            );
+        } else {
+            return <Wrapped {...props} />;
+        }
+    };
+}
