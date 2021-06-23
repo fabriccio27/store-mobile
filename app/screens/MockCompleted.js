e9f8f3845822b8cd6ab25cc25080f22959f8e1e4
fabriccio27 <fabricio_inc27@hotmail.com>
2021-06-23 09:22:39 -0300

    completed MockCompleted, next step, making prettier Items

--- app/screens/MockCompleted.js
+++ app/screens/MockCompleted.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import {StyleSheet, View, Text, ImageBackground} from "react-native";
+import {StyleSheet, View, Text, ImageBackground, TouchableOpacity} from "react-native";
 import LogoutButton from '../components/LogoutButton';
 
-function MockCompleted(props) {
+function MockCompleted({navigation}) {
+
+    const handlePress = ()=>{
+        navigation.navigate("Items");
+    };
+
     return (
         <View style={styles.container}>
             <ImageBackground source={require("../assets/confetiDark.gif")} style={styles.backImage}>
                 <Text style={styles.mainText}>Todo salio bien!</Text>
                 <Text style={styles.footText} numberOfLines={4}>Mas informacion respecto de tu orden esta disponible en tu mail</Text>
 
+                <TouchableOpacity onPress={handlePress} style={styles.touchableButton}>
+                    <Text style={styles.touchableText}>Volver a index</Text>
+                </TouchableOpacity>
             </ImageBackground>
-            
         </View>
-            
         
     );
 };
@@ -39,6 +45,15 @@ let styles = StyleSheet.create({
         fontSize:20,
         width:"60%",
         textAlign:"center"
+    },
+    touchableButton:{
+        backgroundColor:"rgb(54, 145, 97)",
+        borderRadius:10,
+        marginTop:20,
+        padding:10
+    },
+    touchableText:{
+        color:"white"
     }
 });
 
